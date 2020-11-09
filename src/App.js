7e6844572635408901b575566eb89c9b8ae15d88
Sahil <sahilchalia20@gmail.com>
2020-11-09 23:43:15 +0530

    Todo part complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Widgets } from "@material-ui/icons";
+// import { Widgets } from "@material-ui/icons";
 import "./App.css";
 import Feed from "./Feed";
-import Post from "./Post";
+// import Post from "./Post";
 import Sidebar from "./Sidebar";
+import Widgets from './Widgets'
 
 function App() {
   return (
--- src/Widgets.jsx
+++ src/Widgets.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+// import { Widgets } from "@material-ui/icons";
+import React, { useState, useEffect } from "react";
+import AddTodo from './Todo/AddTodo'
+import TodoList from './Todo/TodoList'
+import './Widgets.css'
 
-const Widget = () => {
+const Widgets = () => {
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState("all");
+    const [filteredTodos, setFilteredTodos] = useState([]);
+  
+    useEffect(() => {
+      getFromLocalStorage();
+    }, []);
+  
+    useEffect(() => {
+      console.log("hi");
+      filterHandler();
+      saveToLocalStorage();
+    }, [todos, status]);
+  
+    const filterHandler = () => {
+      switch (status) {
+        case "completed":
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+  
+        case "uncompleted":
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+  
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+  
+    const saveToLocalStorage = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    };
+  
+    const getFromLocalStorage = () => {
+      if (localStorage.getItem("todos") === null) {
+        localStorage.setItem("todos", JSON.stringify([]));
+      } else {
+        let localtodo = JSON.parse(localStorage.getItem("todos"));
+        setTodos(localtodo);
+      }
+    };
     return (
-        <div>
-            <h1>Widget</h1>
-        </div>
-    )
-}
+      <div className="widgets">
+      	<div className="right-todo">
+					<AddTodo 
+						inputText={inputText} 
+						todos={todos} 
+						setTodos={setTodos} 
+						setInputText={setInputText}
+      			setStatus={setStatus} />
 
-export default Widget
+					<TodoList 
+					filteredTodos={filteredTodos} 
+					setTodos={setTodos} 
+					todos={todos} />
+      	</div>
+      	<div className="right-calender">
+      		ygyuy
+      	</div>
+      </div>
+    );
+  };
+  export default Widgets;
+  
